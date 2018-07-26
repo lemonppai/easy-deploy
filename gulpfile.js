3d5a2cae16cbc84499cd687efc5889eff5074223
九林 <lemonppai@qq.com>
2018-07-26 13:59:00 +0800

    v0.4.4

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
 const gulp = require('gulp');
 const GulpSSH = require('gulp-ssh');
 const runSequence = require('gulp-run-sequence');
-const remoteConf = require('./remote.config.js');
+const deployConf = require('./deploy.config.js');
 const fs = require('fs');
 const path = require('path');
 const rootPath = require('app-root-path').path;
 
-const pathToRemote = path.resolve(rootPath, './remote.config.js');
+const pathToRemote = path.resolve(rootPath, './deploy.config.js');
 
 if (fs.existsSync(pathToRemote)) {
-  Object.assign(remoteConf, require(pathToRemote));
+  Object.assign(deployConf, require(pathToRemote));
 }
 
 const gulpSSH = new GulpSSH({
   ignoreErrors: false,
-  sshConfig: remoteConf.ssh
+  sshConfig: deployConf.ssh
 });
 
 
 // 文件上传
 gulp.task('upload', () => {
   return gulp.src(path.resolve(rootPath, './dist/*.zip'))
-    .pipe(gulpSSH.dest(remoteConf.remoteDir));
+    .pipe(gulpSSH.dest(deployConf.remoteDir));
 });
 
 // 解压文件夹到指定目录
 gulp.task('unzip', () => {
-  return gulpSSH.exec(remoteConf.commands);
+  return gulpSSH.exec(deployConf.commands);
 });
 
 
